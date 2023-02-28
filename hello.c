d9fc284f1179d1dfcd0210e32bc778cf5566f7b4
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2023-02-28 21:31:10 +0700

    firs and variable, number

---
+++ hello.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+   printf("Hellooo!\n");
+   printf("    /|\n");
+   printf("   / |\n");
+   printf("  /  |\n");
+   printf(" /___|\n");
+
+   printf("======================================\n");
+   //variables
+   char name[] = "Sarah";
+   int age = 21;
+
+   printf("hii my name is %s\n", name);
+   printf("im %d years old\n", age);
+   printf("i like number %f, and with %s name",500.7 , "Sarahhhh");
+   printf("%f", sqrt(36) );
+
+
+   return 0;
+}
+
+//gcc hello.c -o out
+//./out
+
