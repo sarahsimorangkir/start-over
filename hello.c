a89ebd9160e867b3c2416c78cad0ac82d79810c3
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2023-03-02 09:53:58 +0700

    add array, function, and return type

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
+//gcc hello.c -o out
+//./out
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+//function
+//void is a function that isnt return any type of value/information
+void greetings(char major[], int level){
+    printf("Hello im majoring in %s , and on %d levels\n", major,level);
+}
+double cubing (double num);
 
 int main() {
    printf("Hellooo!\n");
@@ -26,15 +36,27 @@ int main() {
    scanf("%lf", &gpa); //using lf when using scanf
 
    //using fgets and stdin as standadts input if u want to use multiple word
+   //or u can use %s%s to handle this kind of input
    printf("Your GPA is %f\n", gpa);
 //    printf("enter your hobby :");
 //    fgets(hobby, 20, stdin);
 //    printf("Your hobby is %s", hobby);
 
 
+//Array
+    int luckyNumber[]= {1,2,3,4,5,6,7,8};
+    printf("%d\n ", luckyNumber[2]);
+
+    greetings("SWE", 4);
+    printf("The answer of this cubing is %f", cubing(4));
    return 0;
 }
 
-//gcc hello.c -o out
-//./out
+double cubing (double num){
+    return num * num * num;
+}
+
+
+
+
 
